e22fafd6a12bc34ec04745865c7971716c00fd13
zihui <zihui_0411@hotmail.com>
2026-02-11 22:48:36 +0800

    change redirect

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -16,7 +16,7 @@ export default function Dashboard() {
     return (
         <div className=" grid grid-cols-2 w-full h-screen ">
             <SelectionCard title="我要成長" description="技能提升" icon="" image="" ButtonAction={() => RedirectToPage("Growth")} buttonLayout="shadow-[10px_0_20px_2px_rgba(200,80,60,0.7),-10px_0_20px_2px_rgba(230,190,40,0.6)]" subtitle="看見技能 探索學習路徑 找到您的潛力" />
-            <SelectionCard title="我要轉職" description="職涯探索" icon="" image="" ButtonAction={() => RedirectToPage("SwitchCareers")} buttonLayout="shadow-[10px_0_20px_2px_rgba(56,120,130,0.7),-10px_0_20px_2px_rgba(84,117,78,0.6)]" subtitle="探索符合您技能與抱負的內部機會" />
+            <SelectionCard title="我要轉職" description="職涯探索" icon="" image="" ButtonAction={() => RedirectToPage("/")} buttonLayout="shadow-[10px_0_20px_2px_rgba(56,120,130,0.7),-10px_0_20px_2px_rgba(84,117,78,0.6)]" subtitle="探索符合您技能與抱負的內部機會" />
             {!isVisible && <UserBar setVisible={setVisible}/>}
             {<UserProperty isVisible={isVisible} setVisible={setVisible}></UserProperty>}
         </div>
